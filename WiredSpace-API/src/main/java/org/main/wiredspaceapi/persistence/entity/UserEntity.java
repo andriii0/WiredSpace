84947b5e83e0a55ccea17fcabc148b38611aebfe
andriii0 <a.matviienko23@gmail.com>
2025-06-06 01:04:24 +0200

    cascade deleting work  start

--- WiredSpace-API/src/main/java/org/main/wiredspaceapi/persistence/entity/UserEntity.java
+++ WiredSpace-API/src/main/java/org/main/wiredspaceapi/persistence/entity/UserEntity.java
@@ -20,6 +20,24 @@ public class UserEntity extends AccountEntity {
     @Enumerated
     private UserRole role;
 
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<PostEntity> posts = new HashSet<>();
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<PostCommentEntity> comments = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<FriendshipEntity> friendshipsInitiated = new HashSet<>();
+
+    @OneToMany(mappedBy = "friend", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<FriendshipEntity> friendshipsReceived = new HashSet<>();
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<PostLikeEntity> likedPosts = new HashSet<>();
+
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<MessageEntity> sentMessages = new HashSet<>();
+
+    @OneToMany(mappedBy = "recipient", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<MessageEntity> receivedMessages = new HashSet<>();
 }
